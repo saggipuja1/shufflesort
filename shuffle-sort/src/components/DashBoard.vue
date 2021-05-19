5d77444b08abd8abc9a9af3f6295d7e0f3933b53
PUJA <puja.ec676@gmail.com>
2021-05-19 10:41:07 +0530

    img chnages

--- shuffle-sort/src/components/DashBoard.vue
+++ shuffle-sort/src/components/DashBoard.vue
@@ -1,24 +1,46 @@
 <template>
-  <div>
-    <div id="databinding">
-      <span>
-        <button @click="shuffle()">Shuffle</button>
-      </span>
-      <span></span>
-      <span>
-        <button @click="sort()">Sort</button>
-      </span>
-    </div>
-    <div class="cards" v-if="sorted">
-      <div class="card" v-for="item in items" :key="item.id">{{ item }}</div>
+<div>
+  <header>
+          <h1>Shuffle and Sort </h1>
+          </header>
+<div class = "container">
+    <div class = "col bigitem" >
+    <div class="cards" v-if="sorted" >
+      <div class="card" 
+      v-bind:style="{backgroundColor: colors[i]}"
+      :key="i"
+       v-for="(item,i) in items" >{{ item }}</div>
     </div>
     <div class="cards" v-if="shuffled">
       <div class="card" v-for="i in shuffledArray" :key="i.id">{{ i }}</div>
     </div>
   </div>
+<div class = "col smallitem">
+      <div id="databinding  ">
+      <div class = "row flex">
+      <div class = "col">
+        <button class = "button" @click="shuffle()">Shuffle</button>
+      </div>
+      <div class = "col">
+        <button class = "button"   @click="sort()">Sort</button>
+      </div>
+      </div>
+    </div>
+</div>
+    </div>
+  
+      <footer>
+        <h1>Shuffle and sort by Puja Gupta</h1>
+        <div>
+          <span>
+         <img alt="logo maersk" class ="ig" src="../assets/maersk.png"></span>
+       </div>
+  </footer>
+    </div>
 </template>
 
 <script>
+ import image from "../assets/maersk.png"
 export default {
   name: "HelloWorld",
   props: {
@@ -26,10 +48,23 @@ export default {
   },
   data() {
     return {
+      image: image,
       items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
       shuffledArray: null,
       sorted: true,
-      shuffled: false
+      shuffled: false,
+      colors:[
+       "#000000",
+       "#2BBEAD",
+        "#333333",
+        "6F98AB",
+        "#FFFFF",
+        "#BFBFBF",
+        "#EFEFEF",
+        "#2BBEAD",
+        "#72C3DC",
+        ]
+      
     };
   },
   methods: {
@@ -77,7 +112,7 @@ body {
 }
 
 .card {
-  background-color: dodgerblue;
+  background-color: grey;
   color: white;
   padding: 1rem;
   height: 4rem;
@@ -90,4 +125,65 @@ body {
   grid-gap: 1rem;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 }
+.button {
+  background-color:dodgerblue;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  /*grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));*/
+}
+.bigitem {
+   /* This will be twice as */
+   /* big as the small item. 
+  *flex: 2 0 0;*/
+  width:75%;
+
+  }
+.smallitem {
+  *flex: 1 0 0;
+}
+.container {
+  display: flex; /* or inline-flex */
+   flex-wrap: wrap-reverse 
+}
+.bt{
+  display: flex; /* or inline-flex */
+  flex-direction:column-reverse;
+
+} 
+
+.bttn{
+   display: flex; /* or inline-flex */
+  flex-direction:column-reverse;
+
+}
+.wrapper--accessibility-tools {
+  display: flex;
+  flex-wrap: wrap;
+} 
+.fr{
+  float: left;
+}
+@media only screen and (max-width: 600px) {
+.flex{
+   display: flex;
+}
+}
+.xy {
+width: 6em;
+}
+
+.ig{
+  width: 7%;
+    /* float: left; */
+    display: flex;
+    flex-wrap: wrap-reverse;
+}
+
 </style>
